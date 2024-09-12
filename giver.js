08af72b6cf587d01b8197a497fb5d7a5a164f63b
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-09-12 12:37:26 +0530

    Update

---
+++ giver.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+// Function to generate JWT token
+async function genToken(user) {
+  const token = await jwt.sign(user, process.env.SECRET_KEY); // Set token expiration if needed
+  return token;
+}
+
+// Middleware to authenticate the token
+async function auth(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.sendStatus(401); // Unauthorized
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).send("Invalid or expired token"); // Forbidden
+    }
+    req.user = user; // Attach decoded user to req object
+    next(); // Continue to the next middleware or route
+  });
+}
+
+module.exports = { genToken, auth };
