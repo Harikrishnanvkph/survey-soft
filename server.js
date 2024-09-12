ec4e2258554d0a0bad4867127664ec311032bfaa
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-09-12 21:15:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const {MongoClient} = require("mongodb");
-const PORT = process.env.PORT || 3000;
-
 dotenv.config();
 
 const client = new MongoClient(process.env.MONGO_URL);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/',require("./Logic.js"));
@@ -16,15 +15,13 @@ async function startDatabase(){
 	console.log("Database Connected");
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello from Mobile Express Server!');
-});
-
-
 async function startServer(){
 	app.listen(PORT, () => {
         console.log(`Server is running on port http://localhost:${PORT}`);
     });
+    app.get('/', (req, res) => {
+       res.send('Hello from Mobile Express Server!');
+     });
 }
 
 async function init(){
