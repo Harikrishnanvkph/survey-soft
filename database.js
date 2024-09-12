aafcf6d701f51935e20b430e8a274d3ac7bd61a4
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-09-12 19:50:47 +0530

    Update

---
+++ database.js
@@ -0,0 +1,95 @@
+const client = require('server.js');
+
+const dbClient = client.db("Survey"). collection("Users");
+
+//create User
+async function createUser(mail, password){
+    const ce = await dbClient.insertOne({
+mail : mail,
+survey : {
+created : [],
+attended : []}
+});
+    return ce;
+}
+
+// add to surveys list
+async function addSurvey(mail,data){
+    const ce = await client.db("Survey"). collection("AllSurveys").insertOne({
+    	owner : mail,
+        survey_object : data
+});
+    return ce;
+}
+
+// get New Survey No
+async function getId(){
+    const gt =  await db("Survey").collection("TrackIDs").findOne({},{survey_no : 1, _id : 0});
+    return gt.id;
+}
+
+//set new ID
+async function setId(user_id){
+    await db("Survey").collection("TrackIDs").updateOne({},{
+        $set : {
+            survey_no : user_id + 1
+        }
+    })
+}
+
+//show created Surveys
+async function getCreatedSurveys(mail){
+    const ce = await dbClient.findOne({mail : mail},{"survey.created" : 1,_id : 0});
+    return ce;
+}
+
+//create Survey
+async function createSurvey(mail,data){
+	const surveyNo = await getId();
+	const sur = {
+		survey_no : surveyNo,
+        survey_data : data
+     }
+    const ce = await dbClient.findOne({mail : mail},{
+    	{ $push: { "survey.created":  sur} });
+    const addSur = await addSurvey(mail,sur);
+    await setId(surveyNo);
+    return ce;
+}
+
+//add attended Survey
+async function addAttendedSurvey(mail,data,surveyNo){
+	const getSur = await getSurveyNo(surveyNo);
+	if(getSur == null){
+		return "Invalid Survey No"
+		}
+		const sur = {
+		survey_no : surveyNo,
+        survey_data : data
+     }
+    const ce = await dbClient.findOne({mail : mail},{
+    	{ $push: { "survey.attended":  sur} });
+    return ce;
+}
+
+//show attended Surveys
+async function getAttendedSurveys(mail){
+    const ce = await dbClient.findOne({mail : mail},{"survey.attended" : 1,_id : 0});
+    return ce;
+}
+
+
+// search for a survey no
+async function getSurveyNo(survey_no){
+    const ce = await client.db("Survey"). collection("AllSurveys").findOne({
+    	survey_no : survey_no
+});
+    return ce;
+}
+
+
+
+
+module.exports = {createUser,getCreatedSurveys,addSurvey,createSurvey,addAttendedSurvey,
+getAttendedSurveys,addSurvey}
+
