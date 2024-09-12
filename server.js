397fdf57b9f1d01b33171ca577981c28fbc6d2e5
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-09-12 12:36:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(express.json());
+app.use('/',require("./Logic.js"));
 
 app.get('/', (req, res) => {
   res.send('Hello from Mobile Express Server!');
@@ -7,5 +13,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
